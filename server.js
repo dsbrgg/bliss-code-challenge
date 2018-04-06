a84f86c22b00a2d35e547fcc1f54ade07f52c923
opuzzz <dsbrgg@gmail.com>
2018-04-06 02:25:13 +0100

    Fix routing & README

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('view engine', 'hbs');
+//// view engine setup
+//app.set('views', path.join(__dirname, '..', 'views'));
+//app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.render('index');
+    res.sendFile(path.join(__dirname+'/public/index.htm'));
 });
 
 app.listen(3000, () => { console.log('Listening on port 3000...');  });
